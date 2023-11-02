8cc1c23dd09c104cd9228ab525bf58d5b9231952
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-02 21:24:57 +0100

    milestone-2-complete

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 // import {} from './utility.js'
 import {contacts} from './data.js'
+import {getIndex} from './utility.js';
 
 const {createApp} = Vue
 
@@ -9,16 +10,32 @@ createApp({
     data(){
         return {
             contacts: contacts,
+            indexUser: 0
         }
     },
 
     methods: {
         lastMessage(contact){
             return contact.messages[contact.messages.length - 1].message 
-        }
-    },
+        },
 
+        selectById(id){
+            const index = getIndex(id, this.contacts);
+            console.log(index);
+            this.indexUser = index
+        },
+
+    },
+    
     computed: {
+        msgStatusClass(messages){
+            return {
+                'msg_sent' : messages.status === 'sent', 'msg_received' : messages.status === 'received'
+            }
+        },
+
+
+        
         
     },
 
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,8 @@
 
+export const getIndex = function(id, array){
+    return array.findIndex((el) => el.id === id)
+};
+
 function getRndInteger(min, max) 
 {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
